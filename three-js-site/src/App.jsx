fa4f607d64271e440792254beba7db79aee2d293
3nsar <84131267+3nsar@users.noreply.github.com>
2023-04-03 23:06:56 +0200

    commit

--- three-js-site/src/App.jsx
+++ three-js-site/src/App.jsx
@@ -1,11 +1,34 @@
 import { useState } from 'react'
 import './App.css'
+import {Canvas} from "@react-three/fiber"
+import {OrbitControls, PerspectiveCamera, RenderTexture, Sphere, Text} from "@react-three/drei"
 
 function App() {
 
   return (
     <div className="App">
-        <h1>BOX</h1>
+      <div className='container'>
+      <Canvas>
+        <OrbitControls enableZoom={false} autoRotate={true}/>
+        <ambientLight intensity={1}/>
+        <directionalLight position={[3,2,1]}/>
+        <mesh>
+          <boxGeometry  args={[2,2,2]}/>
+          <meshStandardMaterial>
+          <RenderTexture attach="map">
+            <PerspectiveCamera
+              makeDefault
+              position={[0,0,5]}
+            />
+            <color attach="background" args={["red"]}/>
+            <Text fontSize={3} color="#555">
+              hello
+            </Text>
+          </RenderTexture>
+          </meshStandardMaterial>
+        </mesh>
+      </Canvas>
+      </div>
     </div>
   )
 }
