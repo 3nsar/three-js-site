0976a26ec805a493721300ae2a80adbf49791102
3nsar <84131267+3nsar@users.noreply.github.com>
2023-04-25 23:15:35 +0200

    commit

--- three-js-site/src/Navbar.jsx
+++ three-js-site/src/Navbar.jsx
@@ -3,14 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className='nav-container'>
-      <div className='nav-content'>
-        <h1>ENSAR</h1>
-        <ul>
-          <li>About</li>
-          <li>Projects</li>
-          <li>Contact me</li>
-        </ul>
-      </div>
+      <h1>ENSAR</h1>
     </div>
   )
 }
